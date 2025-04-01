1f37328a5c96a0fde2f108454b5662323f2bfdb0
Camilejo <camiloalejocc@gmail.com>
2025-04-01 13:54:44 -0500

    Añadiendo puertos entrantes para los demas metodos

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ app = Flask(__name__)
 
 # Lista de microservicios registrados
 methods = [
-    {"id": 1, "name": "Biseccion", "description": "Encuentra raices por intervalos", "url": "http://localhost:5001/bisection"},
-    {"id": 2, "name": "Newton-Raphson", "description": "Metodo iterativo para hallar raices", "url": "http://localhost:5002/newton_raphson"},
+    {"id": 1, "name": "Punto Fijo", "description": "Método iterativo basado en funciones de iteración", "url": "http://localhost:5001/punto_fijo"},
+    {"id": 2, "name": "Bisección", "description": "Encuentra raíces por intervalos", "url": "http://localhost:5002/bisection"},
     {"id": 3, "name": "Newton-Raphson", "description": "Método iterativo para hallar raíces", "url": "http://localhost:5003/newton_raphson"},
-    {"id": 4, "name": "Secante", "description": "Método numérico para encontrar raíces sin derivadas", "url": "http://localhost:5004/secante"}
+    {"id": 4, "name": "Secante", "description": "Método numérico para encontrar raíces sin derivadas", "url": "http://localhost:5004/secante"},
+    {"id": 4, "name": "jacobi", "description": "Método numérico iterativo para para resolver ecuaciones lineales", "url": "http://localhost:5005/jacobi"},
+    {"id": 4, "name": "gauss-seidel", "description": "Método numérico iterativo para para resolver ecuaciones lineales con presicion albitraria", "url": "http://localhost:5006/gauss-seidel"}
 ]
+
 
 @app.route("/methods", methods=['GET'])
 def get_methods():
